bc2361c426d2fb4d19d54760f6cb505d88948f25
EarlClaro <earlclaro@gmail.com>
2025-01-10 15:14:26 +0800

    deploy

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -22,7 +22,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> EARL JOSEPH CLARO</strong>
+                <strong className="main-name"> EARL JOSEPH A. CLARO</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
